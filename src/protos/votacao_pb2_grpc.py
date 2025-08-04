c27ecc5413903a14c922b20c0a83e19a8847249d
João Victor de Souza Faustino <20221134040001@ifrn.local>
2025-08-04 13:32:29 -0300

    Pequena mudança no arquivo gerado pelo proto

--- src/protos/votacao_pb2_grpc.py
+++ src/protos/votacao_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import votacao_pb2 as votacao__pb2
+from protos import votacao_pb2 as votacao__pb2
 
 GRPC_GENERATED_VERSION = '1.73.0'
 GRPC_VERSION = grpc.__version__
